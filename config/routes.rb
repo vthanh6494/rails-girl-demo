5331f625dc900764339fc65e49cfca359ac4e7c0
Vo Thi Hong Anh <46419913+vthanh6494@users.noreply.github.com>
2019-07-20 16:58:22 +0700

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
+# Rails.application.routes.draw do
+#   get 'products/index'
+#   get 'products/show'
+# end
 Rails.application.routes.draw do
+  root 'products#index'
+  resources :products, only: [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_174808) do
+ActiveRecord::Schema.define(version: 2019_07_20_072509) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.text "name"
+    t.text "brand"
+    t.integer "price"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+  end
+
 end
